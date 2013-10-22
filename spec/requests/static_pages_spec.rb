e9f86ac9cd606e2295833d99e16bc59de93cbbe2
Seth Lipkin <seth.lipkin@gmail.com>
2013-10-22 15:23:14 -0400

    Add more tests and a contact page and integrate with Spork and Guard.

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -23,4 +23,12 @@ describe "StaticPages" do
       expect(page).to have_content('About Us')
     end
   end
+  
+  describe "Contact page" do
+
+    it "should have the title 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_title('Contact')
+    end
+  end
 end

f0201139dad41ac0169518c65f42b22a8bd47bca
Seth Lipkin <seth.lipkin@gmail.com>
2013-10-24 17:18:18 -0400

    Finish layout and routes

--- config/application.rb
+++ config/application.rb
@@ -7,6 +7,7 @@ require "action_mailer/railtie"
 # require "active_resource/railtie"
 require "sprockets/railtie"
 # require "rails/test_unit/railtie"
+require File.expand_path('../boot', __FILE__)
 
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
@@ -57,12 +58,13 @@ module SampleApp
     # This will create an empty whitelist of attributes available for mass-assignment for all models
     # in your app. As such, your models will need to explicitly whitelist or blacklist accessible
     # parameters by using an attr_accessible or attr_protected declaration.
-    config.active_record.whitelist_attributes = true
+    # config.active_record.whitelist_attributes = true
 
     # Enable the asset pipeline
     config.assets.enabled = true
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
   end
 end
